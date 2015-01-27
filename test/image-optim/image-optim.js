f27038650b9e48a99b1f10ff0bcc10b51dc59deb
Evgeniy Gavryushin <job.egavr@yandex.ru>
2015-01-27 17:34:35 +0300

    Add func tests

---
+++ test/image-optim/image-optim.js
@@ -0,0 +1,58 @@
+var fs = require('fs'),
+    mockFs = require('mock-fs'),
+    algorithms = require('./mocks/algorithms'),
+    modes = require('../../lib/modes'),
+    imageOptim = require('../../lib/image-optim');
+
+describe('image-optim', function () {
+    beforeEach(function () {
+        mockFs({
+            '1.ext': new Buffer(10),
+            '2.ext': new Buffer(6)
+        });
+    });
+
+    afterEach(function () {
+        mockFs.restore();
+    });
+
+    describe('optim', function () {
+        it('must optimize files', function (done) {
+            var files = fs.readdirSync('.'),
+                output = [
+                    { name: '1.ext', savedBytes: 1 },
+                    { name: '2.ext', savedBytes: 1 }
+                ];
+
+            imageOptim(files, modes.optim, algorithms)
+                .then(function (res) {
+                    res.must.be.eql(output);
+                })
+                .then(done, done);
+        });
+    });
+
+    describe('lint', function () {
+        it('must lint files', function (done) {
+            var files = fs.readdirSync('.'),
+                output = ['1.ext', '2.ext'];
+
+            imageOptim(files, modes.lint, algorithms)
+                .then(function (res) {
+                    res.must.be.eql(output);
+                })
+                .then(done, done);
+        });
+
+        it('must work option \'tolerance\'', function (done) {
+            var files = fs.readdirSync('.'),
+                output = [];
+
+            imageOptim(files, modes.lint, algorithms, { tolerance: 1 })
+                .then(function (res) {
+                    res.must.be.eql(output);
+                })
+                .then(done, done);
+        });
+    });
+});
